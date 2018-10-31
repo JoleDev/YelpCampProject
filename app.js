77593b163ffc208fc8b9b828f8133886874b30c2
Janith Olagama <janitholagama.info@gmail.com>
2018-10-31 23:24:39 +0400

    Add new files to the repo

--- app.js
+++ app.js
@@ -1,74 +1,49 @@
-var express     = require ("express"),
-    app         = express (),
-    bodyParser  = require ("body-parser"),
-    mongoose    = require ("mongoose");
-
-//These are default code blocks for various npm packages
-app.set ("view engine", "ejs");
-app.use (bodyParser.urlencoded ({extended : true}));
-mongoose.connect ("mongodb://localhost/yelp_camp");
-
-//Defining campGroungSchema
-var campGroungSchema = new mongoose.Schema ({
-    name : String,
-    image : String,
-    description : String
-});
-
-var campGround = mongoose.model ("Campground", campGroungSchema);
-
-app.get ("/", function (req, res) {
-    res.render ("homePage");
-});
-
-// This route will render camp ground name and images from database
-app.get ("/campGrounds", function (req, res) {
-    campGround.find({}, function (err, campGrounds){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render ("campsIndex", {campGrounds : campGrounds});
-        }
-    });
-});
-
-//This is the post route to add new camp grounds, this will add new camp grounds to campgrounds array
-app.post ("/campGrounds", function (req, res) {
-    var campGroundName = req.body.campGroundName;
-    var campGroundImage = req.body.campGroundImage;
-    var campGroundDescription = req.body.campGroundDescription;
-    var newCampGround = {name : campGroundName, image : campGroundImage, description : campGroundDescription};
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
     
-    campGround.create (newCampGround, function (error, campGround) {
-        if (error) {
-            console.log ("Something went worng");
-            console.log (error);
-        } else {
-            //Redirecting to the camp grounds page
-            console.log (campGround);
-            res.redirect ("/campGrounds");
-        }
-    });
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v10");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+// seedDB(); //seed the database
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
 });
 
-//This is the route used to display the form to add new camp grounds
-app.get ("/campGrounds/newCamp", function (req, res) {
-    res.render ("newCamp");
-});
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
-//This is the route used to show camp ground details (Here position of the code is important, For ex. if we put above code
-// /campGrounds/newCamp" below this code every time we try to reach /campGrounds/something it will never reach /campGrounds/newCamp
-// Instead it will render /campGrounds/:id page as it is literally /campGrounds/anything
-app.get ("/campGrounds/:id", function (req, res) {
-    campGround.findById (req.params.id, function (err, campGroundReturned) {
-        if (err) {
-            console.log (err);
-        } else {
-            res.render ("showCamp", {campGround : campGroundReturned});
-        }
-    });
-});
 
-app.listen (3000, function () {
-    console.log ("Yelp Camp Server has initiated");
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
